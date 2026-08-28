72ecada2527a95c97108cf25d1f3268bc604fd1c
@aeg2160 <aeg2160@proton.me>
2026-08-28 16:56:51 -0400

    tailwind works now

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       maxWidth: {
-        site: "1280px"
-      }
-    }
+        site: "1280px",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
 
 export default config;
